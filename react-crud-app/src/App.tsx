fd0bcf6b9113ee04af9d12d4875077de80fdc63e
az-P1-openSUSE <yzhaocl@connect.ust.hk>
2023-09-18 18:25:21 +0800

    ✨ feat: Implement "read" operation

--- react-crud-app/src/App.tsx
+++ react-crud-app/src/App.tsx
@@ -1,35 +1,95 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ReactQuery from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import NProgress from "nprogress";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { readAll } from "./api/noteApi";
+import NoteModal from "./components/note.modal";
+import CreateNote from "./components/notes/create.note";
+import NoteItem from "./components/notes/note.component";
+
+function AppContent() {
+  const [openNoteModal, setOpenNoteModal] = useState(false);
+  const {
+    data: notes,
+    isLoading,
+    isFetching,
+  } = ReactQuery.useQuery({
+    queryKey: ["getNotes"],
+    queryFn: () => readAll(),
+    staleTime: 5 * 1000,
+    select: (data) => data.notes,
+    onSuccess() {
+      NProgress.done();
+    },
+    onError(error: any) {
+      const resMessage =
+        error.response.data.message ||
+        error.response.data.detail ||
+        error.message ||
+        error.toString();
+      toast(resMessage, {
+        type: "error",
+        position: "top-right",
+      });
+      NProgress.done();
+    },
+  });
+  useEffect(() => {
+    if (isLoading || isFetching) {
+      NProgress.start();
+    }
+  }, [isLoading, isFetching]);
+
+  return (
+    <div className="2xl:max-w-[90rem] max-w-[68rem] mx-auto">
+      <div className="m-8 grid grid-cols-[repeat(auto-fill,_320px)] gap-7 grid-rows-[1fr]">
+        <div className="p-4 min-h-[18rem] bg-white rounded-lg border border-gray-200 shadow-md flex flex-col items-center justify-center">
+          <div
+            onClick={() => setOpenNoteModal(true)}
+            className="flex items-center justify-center h-20 w-20 border-2 border-dashed border-ct-blue-600 rounded-full text-ct-blue-600 text-5xl cursor-pointer"
+          >
+            <i className="bx bx-plus"></i>
+          </div>
+          <h4
+            onClick={() => setOpenNoteModal(true)}
+            className="text-lg font-medium text-ct-blue-600 mt-5 cursor-pointer"
+          >
+            Add new note
+          </h4>
+        </div>
+        {/* Note Items */}
+
+        {notes?.map((note) => (
+          <NoteItem key={note.id} note={note} />
+        ))}
+
+        {/* Create Note Modal */}
+        <NoteModal
+          openNoteModal={openNoteModal}
+          setOpenNoteModal={setOpenNoteModal}
+        >
+          <CreateNote setOpenNoteModal={setOpenNoteModal} />
+        </NoteModal>
+      </div>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [queryClient] = useState(() => new ReactQuery.QueryClient());
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ReactQuery.QueryClientProvider client={queryClient}>
+        <AppContent />
+        <ToastContainer />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ReactQuery.QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
