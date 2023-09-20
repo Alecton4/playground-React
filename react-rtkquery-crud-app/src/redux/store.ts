97ae0fb00e54b2b1fb13c042468d149500312320
az-P1-openSUSE <yzhaocl@connect.ust.hk>
2023-09-20 11:01:26 +0800

    ✨ feat: Add CRUD API slice to Redux Store

---
+++ react-rtkquery-crud-app/src/redux/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+import { noteAPI } from "./noteAPI";
+
+// Automatically connects the app to Redux DevTools
+// and adds `redux-thunk` middleware by default to handle asynchronous operations.
+export const store = configureStore({
+  reducer: {
+    [noteAPI.reducerPath]: noteAPI.reducer,
+  },
+  devTools: process.env.NODE_ENV === "development",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([noteAPI.middleware]),
+});
+
+// To enable `refetchOnFocus` and `refetchOnReconnect` behaviors in the app,
+// we passed the dispatch method available on the store to the `setupListeners()` function.
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
